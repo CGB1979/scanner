4c568d328d36155fb79863c0f794011519d7eecd
CGB1979 <cristianbalduzzi@gmail.com>
2026-08-30 22:51:16 -0300

    Update version.js

--- rebotador/js/version.js
+++ rebotador/js/version.js
@@ -1,1 +1,9 @@
-const APP_VERSION = "1.0.1.j";
+const APP_VERSION = "1.0.1.k";
+
+document.addEventListener("DOMContentLoaded", () => {
+    const versionElement = document.getElementById("versionBadge");
+
+    if (versionElement) {
+        versionElement.textContent = `v${APP_VERSION}`;
+    }
+});
