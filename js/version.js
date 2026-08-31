c16ee9f3e6b5c2e2134a0ea4debc5dbceb77c45c
CGB1979 <cristianbalduzzi@gmail.com>
2026-08-30 22:49:19 -0300

    Update version.js

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "1.0.12";
+const APP_VERSION = "1.0.13";
 
 document.addEventListener("DOMContentLoaded", () => {
     const versionElement = document.getElementById("versionBadge");
