44220d7796693ef1b32bcba56599067a9499f7ef
CGB1979 <cristianbalduzzi@gmail.com>
2026-09-02 18:00:25 -0300

    Delete rebotador directory

--- rebotador/js/version.js
+++
@@ -1,9 +0,0 @@
-const APP_VERSION = "1.0.1.l";
-
-document.addEventListener("DOMContentLoaded", () => {
-    const versionElement = document.getElementById("versionBadge");
-
-    if (versionElement) {
-        versionElement.textContent = `v${APP_VERSION}`;
-    }
-});
